e1c070da8983781c83a500e22b4f159bf936338c
QUZIHANWU <quzihanw@student.unimelb.edu.au>
2022-09-19 22:30:29 +1000

    finish income part

--- scripts/external-data-download/download.py
+++ scripts/external-data-download/download.py
@@ -15,8 +15,12 @@ from utils import *
 from urllib.request import urlretrieve
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import gdown
+
 
 output_dir = '../../data/raw/external-data/'
+external_dir = '../../data/external-raw-data/'
+url_suburb_to_postcode = 'https://www.matthewproctor.com/Content/postcodes/australian_postcodes.csv'
 
 class download:
     """
@@ -183,8 +187,120 @@ class download:
         return ()
 
 
-    
+    def total_income():
+        """
+        
+        """
+        # A while loop to run all the pages of this website
+        i=0
+        url = 'http://house.speakingsame.com/suburbtop.php?sta=vic&cat=Median%20household%20income&name=Weekly%20income&page=' + str(i)
+        df = pd.DataFrame()
+        while True:
+            print(url)
+            # find all the table for each page find all the table
+            html_content = requests.get(url).text
+            soup = BeautifulSoup(html_content, "lxml")
+            table = soup.find_all("table")
+            print(len(table))
+            # The income table is the 8th table
+            income_table = table[7]
+            # the head will form our column names
+            body = income_table.find_all("tr")
+            # Head values (Column names) are the first items of the body list
+            head = body[0] # 0th item is the header row
+            body_rows = body[1:] # All other items becomes the rest of the rows
 
+            # Lets now iterate through the head HTML code and make list of clean headings
+
+            # Declare empty list to keep Columns names
+            headings = []
+            for item in head.find_all("td"): # loop through all th elements
+                # convert the th elements to text and strip "\n"
+                item = (item.text).rstrip("\n")
+                # append the clean column name to headings
+                headings.append(item)
+            print(headings)
+            #print(body_rows[0])
+            all_rows = [] # will be a list for list for all rows
+            for row_num in range(len(body_rows)): # A row at a time
+                row = [] # this will old entries for one row
+                for row_item in body_rows[row_num].find_all("td"): #loop through all row entries
+                    # row_item.text removes the tags from the entries
+                    # the following regex is to remove \xa0 and \n and comma from row_item.text
+                    # xa0 encodes the flag, \n is the newline and comma separates thousands in numbers
+                    aa = re.sub("(\xa0)|(\n)|,","",row_item.text)
+                    #append aa to row - note one row entry is being appended
+                    row.append(aa)
+                # append one row to all_rows
+                all_rows.append(row)
+            df_new = pd.DataFrame(data=all_rows,columns=headings)
+            # append the income data of this page to the dataframe
+            df = df.append(df_new, ignore_index=True)
+            # find the next page
+            i = i+1
+            page = requests.get(url)
+            if page.status_code != 200:
+                break
+            # This website only have 13 pages so when run to page 14, break
+            url = 'http://house.speakingsame.com/suburbtop.php?sta=vic&cat=Median%20household%20income&name=Weekly%20income&page=' + str(i)
+            if url == 'http://house.speakingsame.com/suburbtop.php?sta=vic&cat=Median%20household%20income&name=Weekly%20income&page=14':
+                break
+        filename = 'total_income.csv'
+        output_dir_full = f'{external_dir}{filename}'
+        df.to_csv(output_dir_full)
+
+        return ()
+
+
+    @staticmethod
+    def url_suburb_to_postcode():
+        """
+        
+        """
+        output_convert = '../../data/external-raw-data/suburb-to-postcode.csv'
+        gdown.download(url_suburb_to_postcode, output_convert, quiet=False)
+
+
+    @staticmethod
+    def income():
+        """
+        
+        """
+        df_income = pd.read_csv("../../data/external-raw-data/total_income.csv", index_col=0)
+        df_convert = pd.read_csv("../../data/external-raw-data/suburb-to-postcode.csv", index_col=0)
+
+        # Do the preprocessing with convertion file
+        # Take rows for victoria
+        df_convert = df_convert[df_convert["state"] =="VIC"]
+
+        # Only remain the column which are postcode and locality
+        df_convert = df_convert[["postcode", "locality"]]
+
+        # Reset the index
+        df_convert = df_convert.reset_index()
+
+        df_income['Suburb'] = df_income['Suburb'].str.upper()
+
+        df_convert.loc[:,"locality"] = df_convert["locality"].astype(str).str.strip()
+        df_income.loc[:,"Suburb"] = df_income["Suburb"].astype(str).str.strip()
+
+        # inner merge by suburb and locality
+        df_merge = df_income.merge(df_convert, left_on='Suburb', right_on='locality', how='inner')
+
+        df_merge_select = df_merge[["Value","postcode"]]
+
+        df_merge_select['Value'] = df_merge_select['Value'].str[1:]
+
+        df_merge_select["Value"] = pd.to_numeric(df_merge_select["Value"])
+
+        # Group by by the postcode(average each postcode)
+        df_group_by = df_merge_select.groupby(['postcode']).median()
+
+        filename = 'income.csv'
+        output_dir_full = f'{output_dir}{filename}'
+        df_group_by.to_csv(output_dir_full)
+
+        return ()
 
 
     @staticmethod
@@ -193,9 +309,14 @@ class download:
         
         """
 
+
+
         # download.property_and_elector()
         # download.PTV()
         # download.hospital()
         # download.emergency_service()
         # download.public_service()
         # download.care_facility()
+        # download.total_income()
+        # download.url_suburb_to_postcode()
+        # download.income()s
