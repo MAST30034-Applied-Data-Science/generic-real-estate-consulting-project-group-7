d8fe3028e2cc2fad321f7b670480960ac2d1f62d
QUZIHANWU <quzihanw@student.unimelb.edu.au>
2022-09-21 16:47:34 +1000

    idea of school already done

---
+++ scripts/school2/domain_scraper.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+"""
+Domain property web page scrape functions
+"""
+# import libraries
+import json
+from datetime import datetime
+import pickle
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+from constants import headers, home_url
+from utils import domain_property_info, domain_property_links
+from tqdm import tqdm
+import constants
+
+DataFrame = pd.core.frame.DataFrame
+
+
+def domain_properties_info(links: list[str]) -> DataFrame:
+    """
+    Domain properties info scraper
+
+    Args:
+        links (list[str]): list of property links
+        save_schools (bool, optional): save available vic_schools. Defaults to False.
+
+    Returns:
+        DataFrame: domain properties info dataframe
+    """
+    domain_properties = pd.DataFrame()
+
+    for link in links:
+        try:
+            response = requests.get(link, headers=headers, timeout=5).text
+        except requests.exceptions.Timeout:
+            continue
+
+        bs_object = BeautifulSoup(response, "html.parser")
+
+        # obtain all data
+        data = json.loads(bs_object.find("script", {"id": "__NEXT_DATA__"}).text)
+
+        property_df = domain_property_info(data)
+
+        # concatenate property data
+        domain_properties = pd.concat([domain_properties, property_df], axis=0)
+
+    # save the property_df to parquet format
+    domain_properties.reset_index(drop=True).to_parquet(
+        "../../scripts/school2/1/domain_samples.parquet"
+    )
+
+    return domain_properties
+
+
+def domain_links(save_file: bool = False) -> set:
+    """
+    Return valid domain rental property links based on given postcode
+
+    Args:
+        save_file (bool, optional): save the search result to pickle file. Defaults to False.
+
+    Returns:
+        set: domain rental property link set
+    """
+    links = set()
+
+    vic_postcodes = constants.postcodes["VIC"]
+
+    # scrape available property links
+    for i in tqdm(range(len(vic_postcodes))):
+        postcode = vic_postcodes[i]
+
+        print(
+            f"Start scrapping property links belong to postcode: {postcode} links ..."
+        )
+        current_links = domain_property_links(postcode)
+        links = links | current_links
+        print(f"Completed scrapping, postcode {postcode}")
+
+    if save_file:
+        filename = f"{datetime.now().strftime('%Y-%m-%d')}.domain.links.pickle"
+        with open(filename, "wb") as file:
+            pickle.dump(links, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return links
+
+
+def domain_properties_counter(postcodes: int) -> int:
+    """
+    Return the number of maximum possible search results
+
+    Args:
+        postcodes (list): victoria postcodes list
+
+    Returns:
+        int: number of possible results
+    """
+
+    property_counter = 0
+
+    for i in tqdm(range(len(postcodes))):
+        url = home_url["domain"] + f"/rent/?postcode={postcodes[i]}&page=1"
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+        except requests.exceptions.Timeout:
+            continue
+
+        # parse html to lxml format
+        bs_object = BeautifulSoup(response.text, "html.parser")
+
+        data = json.loads(bs_object.find("script", {"id": "__NEXT_DATA__"}).text)[
+            "props"
+        ]
+
+        # count the number of properties
+        property_counter += data["pageProps"]["componentProps"]["totalListings"]
+
+    return property_counter
---
+++ scripts/school2/utils.py
@@ -0,0 +1,183 @@
+"""
+Utility functions module
+"""
+
+# import libraries
+from typing import Union
+from datetime import datetime
+import json
+import pickle
+import time
+
+import requests
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from constants import domain_property_attributes, home_url, headers
+
+
+DataFrame = pd.core.frame.DataFrame
+
+# return maximum response result based on given postcode
+
+
+def domain_result_pages(postcode: int) -> int:
+    """
+    Return maximum domain properties result based on given postcode
+
+    Args:
+        postcode (int): victoria postcode
+
+    Returns:
+        int: a number of maximum page range
+    """
+    url = home_url["domain"] + f"/rent/?postcode={postcode}"
+    try:
+        response = requests.get(url, headers=headers, timeout=5)
+    except requests.exceptions.Timeout:
+        return 0
+
+    bs_object = BeautifulSoup(response.text, "html.parser")
+    page_props = json.loads(bs_object.find("script", {"id": "__NEXT_DATA__"}).text)[
+        "props"
+    ]["pageProps"]
+
+    return (
+        page_props["layoutProps"]["digitalData"]["page"]["pageInfo"]["search"][
+            "resultsPages"
+        ]
+        + 1
+    )
+
+
+# return unique extracted link for given postcode
+def domain_property_links(
+    postcode: int, delay: int = 0, save_file: bool = False
+) -> set:
+    """
+    Obtain property links based on given victoria postcode
+
+    Args:
+        postcode (int): victoria postcode
+
+    Returns:
+        set: set of domain property list
+    """
+    # result links
+    property_links = set()
+
+    for page in tqdm(np.arange(1, domain_result_pages(postcode))):
+        url = home_url["domain"] + f"/rent/?postcode={postcode}&page={page}"
+
+        # parsing html to lxml format
+        try:
+            response = requests.get(url, headers=headers, timeout=10)
+        except requests.exceptions.Timeout:
+            continue
+
+        # parse the http response
+        bs_object = BeautifulSoup(response.text, "html.parser")
+
+        # obtain NextJS data
+        data = json.loads(bs_object.find("script", {"id": "__NEXT_DATA__"}).text)[
+            "props"
+        ]
+        page_props = data["pageProps"]
+        component_props = page_props["componentProps"]
+        listings_map = component_props["listingsMap"]
+
+        if listings_map:
+            for prop in listings_map:
+                url = home_url["domain"] + listings_map[prop]["listingModel"]["url"]
+                property_links.add(url)
+        else:
+            continue
+
+        if delay:
+            time.sleep(delay)
+
+    # dump current url data into pickle file
+    if save_file:
+        filename = f"{datetime.now().strftime('%Y-%m-%d')} {postcode}.domain.pickle"
+        with open(filename, "wb") as file:
+            pickle.dump(property_links, file, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return property_links
+
+
+def domain_nearby_schools(component_props: dict) -> list:
+    """
+    Obtain school names based on given target property info page
+
+    Args:
+        componentProps (dict): webpage component description information
+
+    Returns:
+        list: list of nearby school names of a given property
+    """
+    if "schoolCatchment" in component_props:
+        if "schools" in component_props["schoolCatchment"]:
+            return list(
+                s["name"] for s in component_props["schoolCatchment"]["schools"]
+            )
+        else:
+            return list()
+
+
+def domain_nearby_schools_distance(component_props: dict) -> list:
+    """
+    Obtain school names based on given target property info page
+
+    Args:
+        componentProps (dict): webpage component description information
+
+    Returns:
+        list: list of nearby school names of a given property
+    """
+    if "schoolCatchment" in component_props:
+        if "schools" in component_props["schoolCatchment"]:
+            return list(
+                s["distance"] for s in component_props["schoolCatchment"]["schools"]
+            )
+        else:
+            return list()
+
+
+def domain_property_info(data: dict) -> Union[DataFrame, None]:
+    """
+    Obtain single domain property information
+
+    Args:
+        url (str): given domain property target web address
+
+    Returns:
+        pd.core.frame.DataFrame: a dataframe contains property information
+    """
+    # obtain required data from pageProps
+    page_props = data["props"]["pageProps"]
+
+    # property data object
+    digital_data = page_props["layoutProps"]["digitalData"]
+    component_props = page_props["componentProps"]  # school data object
+
+    # property information
+    property_df = pd.DataFrame.from_dict(
+        digital_data["page"]["pageInfo"]["property"], orient="index"
+    ).transpose()[domain_property_attributes]
+
+    # obtain geolocation info
+    # property_df["latitude"] = component_props["map"]["latitude"]
+    # property_df["longitude"] = component_props["map"]["longitude"]
+
+    # school information
+    property_df["nearBySchools"] = property_df.apply(
+        lambda x: domain_nearby_schools(component_props), axis=1
+    )
+
+    property_df["nearBySchools_distance"] = property_df.apply(
+        lambda x: domain_nearby_schools_distance(component_props), axis=1
+    )
+
+    return property_df
